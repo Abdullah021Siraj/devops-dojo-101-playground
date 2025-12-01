a898a652f8e5e967b63187e651c1d99037cc1cf5
Abdullah Siraj <dev.abdullah21@gmail.com>
2025-12-01 21:16:23 +0500

    Update and rename docker-nginx-intstallation to docker-nginx-intstallation.sh

--- templates/docker-nginx-intstallation.sh
+++ templates/docker-nginx-intstallation.sh
@@ -6,7 +6,10 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyring
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add repo (adjust if you’re on Debian)
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://d>
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
